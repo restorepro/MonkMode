629b258e2a9a9cb09d21f096e22a3b00c9356cb0
restorepro <service@floodtechs.com>
2025-09-11 19:30:18 -0400

    Initial Commit

---
+++ MonkMode/MonkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonkModeApp.swift
+//  MonkMode
+//
+//  Created by Greg Williams on 11.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct MonkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
